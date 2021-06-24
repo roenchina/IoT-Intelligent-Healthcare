c380b6afe44039242c1dfa71a7324bcca9110273
yuruohan <1005623825@qq.com>
2021-06-24 16:22:54 +0800

    删除原生echarts引入

--- src/main.js
+++ src/main.js
@@ -6,26 +6,26 @@ import axios from 'axios'
 import ElementPlus from 'element-plus'
 
 // echarts
-import Echarts from 'vue-echarts'
-import {use} from 'echarts/core'
+// import Echarts from 'vue-echarts'
+// import {use} from 'echarts/core'
 
-import {
-  CanvasRenderer
-} from 'echarts/renderers'
-import {
-  BarChart
-} from 'echarts/charts'
-import {
-  GridComponent,
-  TooltipComponent
-} from 'echarts/components'
+// import {
+//   CanvasRenderer
+// } from 'echarts/renderers'
+// import {
+//   BarChart
+// } from 'echarts/charts'
+// import {
+//   GridComponent,
+//   TooltipComponent
+// } from 'echarts/components'
 
-use([
-  CanvasRenderer,
-  BarChart,
-  GridComponent,
-  TooltipComponent
-]);
+// use([
+//   CanvasRenderer,
+//   BarChart,
+//   GridComponent,
+//   TooltipComponent
+// ]);
 
 // element-ui
 import 'element-plus/lib/theme-chalk/index.css'
@@ -37,6 +37,5 @@ app.config.globalProperties.$axios = axios
 app.use(store)
    .use(router)
    .use(ElementPlus)
-   .component('v-charts', Echarts)
-   // .use(AmapVue)
+  //  .component('v-charts', Echarts)
    .mount('#app')
