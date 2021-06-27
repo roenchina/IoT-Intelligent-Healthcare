892b476a7cec017eab1450bfbd54a19b0c069809
yuruohan <53894071+roenchina@users.noreply.github.com>
2021-06-27 23:51:16 +0800

    [Flask-API] 第一个数据库查询接口测试成功

--- back-end/flask/api/charts.py
+++ back-end/flask/api/charts.py
@@ -1,14 +1,24 @@
 from flask import Blueprint, request, jsonify
 from database.extension import db
-from database.models import *
+from database.models import User, Facility, Data, Own
+from api.format import error_response, bad_request, common_response
+
 
 charts_bp = Blueprint("charts_bp", __name__, url_prefix='/charts')
 
 
 @charts_bp.route('/getFacPie', methods=["GET"])
 def getFacPie():
+    return_data = []
+    for type in ['online', 'offline', 'error']:
+        count = Facility.query.filter_by(status=type).count()
+        item = {
+            'name': type,
+            'value': count
+        }
+        return_data.append(item)
     
-    return 'api: getFacPie'
+    return common_response(return_data)
 
 
 @charts_bp.route('/getDataPie', methods=["GET"])
