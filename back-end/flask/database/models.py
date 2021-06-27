c01d801aab17be3320c6f7ac9075d9b24119e0ba
yuruohan <53894071+roenchina@users.noreply.github.com>
2021-06-27 23:10:55 +0800

    [Flask-DB] 修改Models，增加sql插入数据

--- back-end/flask/database/models.py
+++ back-end/flask/database/models.py
@@ -2,20 +2,6 @@ from datetime import datetime
 
 from database.extension import db
 
-# class User(db.Model):
-#     __tablename__ = 'user'
-#     id = db.Column(db.Integer, primary_key=True)
-#     cash = db.Column(db.Integer)
-
-#     def __init__(self, name, thrust):
-#         self.name = name
-#         self.thrust = thrust
-
-#     def __repr__(self):
-#         return '<User %r>' % self.name
-
-#     def __str__(self):
-#         return '<User %s>' % self.name
 
 class User(db.Model):
     __tablename__ = 'tb_user'
@@ -65,7 +51,7 @@ class Data(db.Model):
 
 class Own(db.Model):
     __tablename__ = 'tb_own'
-    id = db.Column(db.String(10), primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
     # userID = db.Column(db.String(10), nullable=False)
     wardID = db.Column(db.String(10), nullable=False)
     bedID = db.Column(db.String(10), nullable=False)
--- back-end/flask/database/schema.sql
+++ back-end/flask/database/schema.sql
@@ -52,3 +52,46 @@ CREATE TABLE `tb_own` (
   KEY `userID` (`userID`),
   CONSTRAINT `tb_own_ibfk_1` FOREIGN KEY (`userID`) REFERENCES `tb_user` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- facility
+INSERT INTO `tb_facility` VALUES ('10001', '201病房温度监控', 'temp', 'online', 'Celsius', '60', '201', '00');
+INSERT INTO `tb_facility` VALUES ('20001', '201病房湿度监控', 'humi', 'online', '%', '60', '201', '00');
+INSERT INTO `tb_facility` VALUES ('30011', '201-01床体温', 'bodyt', 'online', 'Celsius', '30', '201', '01');
+INSERT INTO `tb_facility` VALUES ('40011', '201-01床心率', 'rate', 'online', 'BPM', '5', '201', '01');
+INSERT INTO `tb_facility` VALUES ('30012', '201-02床体温', 'bodyt', 'online', 'Celsius', '30', '201', '02');
+INSERT INTO `tb_facility` VALUES ('40012', '201-02床心率', 'rate', 'online', 'BPM', '5', '201', '02');
+
+INSERT INTO `tb_facility` VALUES ('10008', '208病房温度监控', 'temp', 'error', 'Celsius', '60', '208', '00');
+INSERT INTO `tb_facility` VALUES ('20008', '208病房湿度监控', 'humi', 'offline', '%', '60', '208', '00');
+INSERT INTO `tb_facility` VALUES ('30081', '208-01床体温', 'bodyt', 'offline', 'Celsius', '30', '208', '01');
+
+-- data
+INSERT INTO `tb_data` VALUES ('1000101', '2021-06-27 08:20:00', 120.131042, 30.273052, 'normal', '27.1', '10001');
+INSERT INTO `tb_data` VALUES ('1000102', '2021-06-27 08:50:00', 120.130642, 30.272852, 'normal', '24.2', '10001');
+INSERT INTO `tb_data` VALUES ('1000103', '2021-06-27 09:20:00', 120.132142, 30.272352, 'normal', '26.6', '10001');
+INSERT INTO `tb_data` VALUES ('1000104', '2021-06-27 09:50:00', 120.131342, 30.274252, 'normal', '26.8', '10001');
+INSERT INTO `tb_data` VALUES ('1000105', '2021-06-27 10:20:00', 120.132042, 30.274052, 'normal', '27.2', '10001');
+INSERT INTO `tb_data` VALUES ('1000106', '2021-06-27 10:50:00', 120.131842, 30.274752, 'normal', '26.9', '10001');
+INSERT INTO `tb_data` VALUES ('1000107', '2021-06-27 11:20:00', 120.130742, 30.274952, 'normal', '27.8', '10001');
+INSERT INTO `tb_data` VALUES ('1000108', '2021-06-27 11:50:00', 120.130642, 30.273552, 'normal', '28.4', '10001');
+
+INSERT INTO `tb_data` VALUES ('2000101', '2021-06-27 08:20:00', 120.131642, 30.273852, 'warning', '00', '20001');
+INSERT INTO `tb_data` VALUES ('2000102', '2021-06-27 08:50:00', 120.130342, 30.273252, 'normal', '65', '20001');
+INSERT INTO `tb_data` VALUES ('2000103', '2021-06-27 09:20:00', 120.131642, 30.272552, 'normal', '73', '20001');
+INSERT INTO `tb_data` VALUES ('2000104', '2021-06-27 09:50:00', 120.130042, 30.272052, 'normal', '72', '20001');
+INSERT INTO `tb_data` VALUES ('2000105', '2021-06-27 10:20:00', 120.131042, 30.273052, 'warning', '98', '20001');
+INSERT INTO `tb_data` VALUES ('2000106', '2021-06-27 10:50:00', 120.131142, 30.274352, 'normal', '79', '20001');
+
+INSERT INTO `tb_data` VALUES ('3001101', '2021-06-27 8:00:00', 120.133142, 30.274352, 'normal', '36.5', '30011');
+INSERT INTO `tb_data` VALUES ('3001102', '2021-06-27 8:30:00', 120.132142, 30.275352, 'normal', '36.8', '30011');
+
+INSERT INTO `tb_data` VALUES ('4001101', '2021-06-27 8:05:00', 120.133142, 30.274460, 'normal', '80', '40011');
+INSERT INTO `tb_data` VALUES ('4001102', '2021-06-27 8:10:00', 120.132142, 30.275952, 'normal', '82', '40011');
+
+-- user
+INSERT INTO `tb_user` VALUES ('3180105412', 'Yu Ruohan(test_manager)', "3180105412@zju.edu.cn", '123456', 'manager');
+INSERT INTO `tb_user` VALUES ('3180101111', 'test_patient', "12345678@example.com", '123456', 'patient');
+INSERT INTO `tb_user` VALUES ('3180102222', 'test_doctor', "87654321@example.com", '123456', 'doctor');
+
+-- own
+INSERT INTO `tb_own` (`wardID`, `bedID`, `userID`) VALUES ('201', "01", '3180101111');
