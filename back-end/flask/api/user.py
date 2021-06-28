e400efeef2af4e4706b358bceb3e4ddcdc45eceb
yuruohan <53894071+roenchina@users.noreply.github.com>
2021-06-28 12:23:34 +0800

    [Flask-API] 后端user api完成

--- back-end/flask/api/user.py
+++ back-end/flask/api/user.py
@@ -1,14 +1,61 @@
-from flask import Blueprint
+from flask import Blueprint, request
+from database.models import User, Facility, Data, Own
+from api.format import error_response, bad_request, common_response
+from database.extension import db
 
 user_bp = Blueprint("user_bp", __name__, url_prefix='/user')
 
 
 @user_bp.route('/userRegister', methods=["POST"])
 def userRegister():
-    return 'api: userRegister'
+    data = {}
+    request_json = request.get_json()
+    res = User.query.get(request_json['userID'])
+    if(res):
+        data = {
+            'ifTrue': False,
+            'message': 'userID already exists'
+        }
+    else:
+        newuser = User(id=request_json['userID'],
+                       name=request_json['name'],
+                       email=request_json['email'],
+                       passwd=request_json['passwd'],
+                       role=request_json['role'])
+        db.session.add(newuser)
+        db.session.commit()
+        data = {
+            'ifTrue': True,
+            'message': 'add newuser success'
+        }
+    return common_response(data)
 
 
 @user_bp.route('/userVerify', methods=["POST"])
 def userVerify():
-    return 'api: userVerify'
+    data = {}
+    request_json = request.get_json()
+    res = User.query.get(request_json['userID'])
+    if (res):
+        if (res.passwd == request_json['passwd']):
+            data = {
+                'ifTrue': True,
+                'userInfo': {
+                    'name': res.name,
+                    'email': res.email,
+                    'role': res.role
+                },
+                'message': 'success'
+            }
+        else:
+            data = {
+                'ifTrue': False,
+                'message': 'wrong passwd'
+            }
+    else:
+        data = {
+            'ifTrue': False,
+            'message': 'userID not exists'
+        }
 
+    return common_response(data)
